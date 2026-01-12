6c0d2e56e5d0cecd3cb40ea623d75897060d7a49
LucaCiani <lucaciani88@gmail.com>
2026-01-12 17:15:37 +0100

    fetch problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function App() {
+    const [iphones, setIphones] = useState([]);
+
+    fetch("http://localhost:3001" + "/iphones")
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+
     return <></>;
 }
 
