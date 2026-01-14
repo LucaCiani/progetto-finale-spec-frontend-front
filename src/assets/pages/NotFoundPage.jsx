6a3251cb940faeea001e471ba4725b0ec5b2353c
LucaCiani <lucaciani88@gmail.com>
2026-01-14 22:59:07 +0100

    fix not found page

--- src/assets/pages/NotFoundPage.jsx
+++ src/assets/pages/NotFoundPage.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFoundPage() {
+    const navigate = useNavigate();
     return (
         <>
-            <h2 className="text-center py-5">Prodotto non trovato!</h2>
+            <div className="d-flex flex-column justify-content-center">
+                <h2 className="text-center py-5">Prodotto non trovato!</h2>
+                <button
+                    className="mx-auto btn btn-primary"
+                    onClick={() => navigate("/")}
+                >
+                    Torna alla Home
+                </button>
+            </div>
         </>
     );
 }
